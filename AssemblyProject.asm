6da36a621bea6541afdab1cf568a227cbe67c6a6
Youssef Abdelrahman <162054447+Enovac@users.noreply.github.com>
2024-05-22 12:53:10 +0200

    Fixed error

--- AssemblyProject.asm
+++ AssemblyProject.asm
@@ -1,22 +1,35 @@
 org 100h
-;Conventions used in this code
-;All variables,labels are camelcased                              
-;All instructions,registers are fully capitalized
-;procs,macros first letter each word capitalzied                          
-.Data  
-  SBOX DB 63H,7CH,77H, 7BH, 0F2H, 6BH, 6FH, 0C5H, 30H, 01H, 67H, 2BH, 0FEH, 0D7H, 0ABH, 76H, 0CAH, 82H, 0C9H, 7DH, 0FAH, 59H, 47H, 0F0H, 0ADH, 0D4H, 0A2H, 0AFH, 9CH, 0A4H, 72H, 0C0H, 0B7H, 0FDH, 93H, 26H, 36H, 3FH, 0F7H, 0CCH, 34H, 0A5H, 0E5H, 0F1H, 71H, 0D8H, 31H, 15H, 04H, 0C7H, 23H, 0C3H, 18H, 96H, 05H, 9AH, 07H, 12H, 80H, 0E2H, 0EBH, 27H, 0B2H, 75H, 09H, 83H, 2CH, 1AH, 1BH, 6EH, 5AH, 0A0H, 52H, 3BH, 0D6H, 0B3H, 29H, 0E3H, 2FH, 84H, 53H, 0D1H, 00H, 0EDH, 20H, 0FCH, 0B1H, 5BH, 6AH, 0CBH, 0BEH, 39H, 4AH, 4CH, 58H, 0CFH, 0D0H, 0EFH, 0AAH, 0FBH, 43H, 4DH, 33H, 85H, 45H, 0F9H, 02H, 7FH, 50H, 3CH, 9FH, 0A8H, 51H, 0A3H, 40H, 8FH, 92H, 9DH, 38H, 0F5H, 0BCH, 0B6H, 0DAH, 21H, 10H, 0FFH, 0F3H, 0D2H, 0CDH, 0CH, 13H, 0ECH, 5FH, 97H, 44H, 17H, 0C4H, 0A7H, 7EH, 3DH, 64H, 5DH, 19H, 73H, 60H, 81H, 4FH, 0DCH, 22H, 2AH, 90H, 88H, 46H, 0EEH, 0B8H, 14H, 0DEH, 5EH, 0BH, 0DBH, 0E0H, 32H, 3AH, 0AH, 49H, 06H, 24H, 5CH, 0C2H, 0D3H, 0ACH, 62H, 91H, 95H, 0E4H, 79H, 0E7H, 0C8H, 37H, 6DH, 8DH, 0D5H, 4EH, 0A9H, 6CH, 56H, 0F4H, 0EAH, 65H, 7AH, 0AEH, 08H, 0BAH, 78H, 25H, 2EH, 1CH, 0A6H, 0B4H, 0C6H, 0E8H, 0DDH, 74H, 1FH, 4BH, 0BDH, 8BH, 8AH, 70H, 3EH, 0B5H, 66H, 48H, 03H, 0F6H, 0EH, 61H, 35H, 57H, 0B9H, 86H, 0C1H, 1DH, 9EH, 0E1H, 0F8H, 98H, 11H, 69H, 0D9H, 8EH, 94H, 9BH, 1EH, 87H, 0E9H, 0CEH, 55H, 28H, 0DFH, 8CH, 0A1H, 89H, 0DH, 0BFH, 0E6H, 42H, 68H, 41H, 99H, 2DH, 0FH, 0B0H, 54H, 0BBH, 16H
- 
-  SBOXRows  EQU 16
-  SBOXCol   EQU 16
+.DATA
 
+  SBOX	DB 63H,7cH,77H,7bH,0f2H,6bH,6fH,0c5H,30H,01H,67H,2bH,0feH,0d7H,0abH,76H
+     	DB 0caH,82H,0c9H,7dH,0faH,59H,47H,0f0H,0adH,0d4H,0a2H,0afH,9cH,0a4H,72H,0c0H
+     	DB 0b7H,0fdH,93H,26H,36H,3fH,0f7H,0ccH,34H,0a5H,0e5H,0f1H,71H,0d8H,31H,15H
+     	DB 04H,0c7H,23H,0c3H,18H,96H,05H,9aH,07H,12H,80H,0e2H,0ebH,27H,0b2H,75H
+     	DB 09H,83H,2cH,1aH,1bH,6eH,5aH,0a0H,52H,3bH,0d6H,0b3H,29H,0e3H,2fH,84H
+     	DB 53H,0d1H,00H,0edH,20H,0fcH,0b1H,5bH,6aH,0cbH,0beH,39H,4aH,4cH,58H,0cfH
+     	DB 0d0H,0efH,0aaH,0fbH,43H,4dH,33H,85H,45H,0f9H,02H,7fH,50H,3cH,9fH,0a8H
+     	DB 51H,0a3H,40H,8fH,92H,9dH,38H,0f5H,0bcH,0b6H,0daH,21H,10H,0ffH,0f3H,0d2H
+    	DB 0cdH,0cH,13H,0ecH,5fH,97H,44H,17H,0c4H,0a7H,7eH,3dH,64H,5dH,19H,73H
+     	DB 60H,81H,4fH,0dcH,22H,2aH,90H,88H,46H,0eeH,0b8H,14H,0deH,5eH,0bH,0dbH
+     	DB 0e0H,32H,3aH,0aH,49H,06H,24H,5cH,0c2H,0d3H,0acH,62H,91H,95H,0e4H,79H
+     	DB 0e7H,0c8H,37H,6dH,8dH,0d5H,4eH,0a9H,6cH,56H,0f4H,0eaH,65H,7aH,0aeH,08H
+     	DB 0baH,78H,25H,2eH,1cH,0a6H,0b4H,0c6H,0e8H,0ddH,74H,1fH,4bH,0bdH,8bH,8aH
+     	DB 70H,3eH,0b5H,66H,48H,03H,0f6H,0eH,61H,35H,57H,0b9H,86H,0c1H,1dH,9eH
+     	DB 0e1H,0f8H,98H,11H,69H,0d9H,8eH,94H,9bH,1eH,87H,0e9H,0ceH,55H,28H,0dfH
+     	DB 8cH,0a1H,89H,0dH,0bfH,0e6H,42H,68H,41H,99H,2dH,0fH,0b0H,54H,0bbH,16H
+   	 
+  SBOXRows  EQU 16
+  SBOXCol   EQU 16 
+;==================================================================
   totalColNum EQU 4
   totalRowNum EQU 4
-  state  DB 0d4H,0e0H,0b8H,1eH,27H,0bfH,0b4H,41H,11H,98H,5dH,52H,0aeH,0f1H,0e5H,30H  ;Input Array State  
-  ;algorithm to access row x col y =(currentRow*colNum)+currentCol
+  state  DB 19H,0A0H,9AH,0E9H,3DH,0F4H,0C6H,0F8H,0E3H,0E2H,8DH,48H,0BEH,2BH,2AH,08H  
 
- 
-.Code  
- ShiftRows MACRO array 
+  ;algorithm to access row x col y =(currentRow*colNum)+currentCol
+;===============================================================================================     
+.CODE  
+
+ShiftRows MACRO array 
  
     MOV DI,1 ;Used For RowIndex Start with 1 
     ;SI Will be Used For Col Index
@@ -61,7 +74,9 @@ org 100h
     CMP DI,totalColNum
     JNZ NextRowLoop     
     
- ShiftRows ENDM 
+ ShiftRows ENDM   
+;==================================================================
+  
   AddRoundKey MACRO st,k
     Mov SI,0 ;XORING START AT 0  
     Mov AX,totalColNum
@@ -75,9 +90,9 @@ org 100h
     INC SI
     LOOP StartLoop      
                         
-AddRoundKey ENDM
-
-;SUB BYTES WORKS AS LOGIC HOWEVER DUE TO SIZE OF SBOX ERROR OCCURS IF KEY STARTS WITH LETTER
+AddRoundKey ENDM 
+ 
+;================================================================== 
 SubBytes MACRO k,sb
 ;For this function we replace all keys with proper SBOX values
 ;The idea is if given 19 the 1 is the row of the SBOX and the 9 is the colmn
@@ -94,7 +109,6 @@ MOV BL,totalRowNum
 Mul BL
 MOV CX,AX
 MOV SI,0
-MOV BP,OFFSET sb  ;This is becasue directly using sb[,,,] gives error 
 StartLoop: 
 MOV DL,k[SI]
 MOV BL,DL
@@ -106,7 +120,7 @@ MOV AX,SBOXCol
 MUL DX
 ADD AX,BX
 MOV DI,AX
-MOV AL,BP+DI
+MOV AL,sb[DI]
 MOV k[SI],AL
 
 
@@ -116,15 +130,17 @@ MOV k[SI],AL
 INC SI    
 LOOP StartLoop    
     
-SubBytes ENDM 
+SubBytes ENDM  
+;================================================================== 
+SubBytes state,SBOX 
+ 
+    
+ret      
+
 
  
 
-
- 
-ret
-
-
+    
 
 
 
